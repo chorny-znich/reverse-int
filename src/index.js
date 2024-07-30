4eecc5bf2722c7e3275f0e37f835473a44ce1bd8
chorny-znich <vargar.dt@gmail.com>
2024-07-30 09:53:32 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(Math.abs(n));
+    const arr = str.split("");
+    if (arr.length === 1) {
+        return n;
+    }
+    let result = "";
+    const startIndex =
+        arr[arr.length - 1] === "0" ? arr.length - 2 : arr.length - 1;
+    for (let i = startIndex; i >= 0; i--) {
+        result += arr[i];
+    }
+    return result;
+};
